84b9f69640b1865a55d1514a953dd9d7c71dd878
wiktor <wiktor.jurasz@gmail.com>
2016-05-14 13:08:11 +0200

    Commented out unnecessary middleware functions, minor refactoring

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+// var favicon = require('serve-favicon');
+// var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var location = require('./controllers/location');
+var users = require('./controllers/users');
+var auth = require('./controllers/auth');
 
 var app = express();
 
@@ -16,15 +17,16 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/auth', auth);
 app.use('/users', users);
+app.use('/location', location);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ controllers/auth.js
@@ -0,0 +1,8 @@
+/**
+ *
+ * Created by wiktor on 14/05/16.
+ */
+var express = require('express');
+var router = express.Router();
+
+module.exports = router;
