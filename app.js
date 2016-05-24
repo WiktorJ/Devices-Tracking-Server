f5b4ceacc0176ec85e2b0d147d263aac6526917f
bgrochal <bartek.grochal@gmail.com>
2016-05-24 20:17:07 +0200

    Updated Access-Control headers for Heroku deployment.

--- app.js
+++ app.js
@@ -10,13 +10,21 @@ var location = require('./domain/location/index');
 var users = require('./domain/users/index');
 var auth = require('./auth/index');
 
-
-
 var app = express();
+
+
+// add response headers, which allow to communicate between different hosts.
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -27,16 +35,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 // app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// additional custom scripts
 app.use('/auth', auth);
 app.use('/users', users);
 app.use('/location', location);
-
-// Add response headers, which allow to communicate between different hosts.
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 
 // catch 404 and forward to error handler
@@ -45,6 +48,7 @@ app.use(function (req, res, next) {
     err.status = 404;
     next(err);
 });
+
 
 // error handlers
 
